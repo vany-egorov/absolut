7659d65d15010a0ace0ba6a7c41b573874df3eb6
Ivan Egorov <vany.egorov@gmail.com>
2015-02-02 12:51:51 +0000

    autocommit fix

--- log_stack.go
+++ log_stack.go
@@ -5,20 +5,25 @@ import (
 	"fmt"
 	"sync"
 
-	log "github.com/cihub/seelog"
+	"github.com/cihub/seelog"
 )
+
+type loggerGetter interface {
+	GetLogger() seelog.LoggerInterface
+}
 
 type LogStack struct {
 	sync.Mutex
-	log      log.LoggerInterface
+
+	log          seelog.LoggerInterface
+	loggerGetter loggerGetter
+
 	s        []logMesage
 	isClosed bool
 }
 
 type logMesage struct {
-	log.LoggerInterface
-
-	level log.LogLevel
+	level seelog.LogLevel
 	text  string
 }
 
@@ -27,14 +32,17 @@ func LogStackNew(args ...interface{}) *LogStack {
 	self.isClosed = false
 
 	if len(args) > 0 {
-		if v, ok := args[0].(log.LoggerInterface); ok {
+		if v, ok := args[0].(seelog.LoggerInterface); ok {
 			self.log = v
+		}
+		if v, ok := args[0].(loggerGetter); ok {
+			self.loggerGetter = v
 		}
 	} else {
 		if defautLogger := GetDefaultLogger(); defautLogger != nil {
 			self.log = defautLogger
 		} else {
-			self.log = log.Current
+			self.log = seelog.Current
 		}
 	}
 
@@ -42,41 +50,41 @@ func LogStackNew(args ...interface{}) *LogStack {
 }
 
 func (self *LogStack) Tracef(format string, params ...interface{}) {
-	self.appendWithLevelf(log.TraceLvl, format, params...)
+	self.appendWithLevelf(seelog.TraceLvl, format, params...)
 }
 
 func (self *LogStack) Debugf(format string, params ...interface{}) {
-	self.appendWithLevelf(log.DebugLvl, format, params...)
+	self.appendWithLevelf(seelog.DebugLvl, format, params...)
 }
 
 func (self *LogStack) Infof(format string, params ...interface{}) {
-	self.appendWithLevelf(log.InfoLvl, format, params...)
+	self.appendWithLevelf(seelog.InfoLvl, format, params...)
 }
 
 func (self *LogStack) Warnf(format string, params ...interface{}) error {
-	return errors.New(self.appendWithLevelf(log.WarnLvl, format, params...).text)
+	return errors.New(self.appendWithLevelf(seelog.WarnLvl, format, params...).text)
 }
 
 func (self *LogStack) Errorf(format string, params ...interface{}) error {
-	return errors.New(self.appendWithLevelf(log.ErrorLvl, format, params...).text)
+	return errors.New(self.appendWithLevelf(seelog.ErrorLvl, format, params...).text)
 }
 
 func (self *LogStack) Criticalf(format string, params ...interface{}) error {
-	return errors.New(self.appendWithLevelf(log.CriticalLvl, format, params...).text)
+	return errors.New(self.appendWithLevelf(seelog.CriticalLvl, format, params...).text)
 }
 
-func (self *LogStack) Trace(v ...interface{}) { self.appendWithLevel(log.TraceLvl, v...) }
-func (self *LogStack) Debug(v ...interface{}) { self.appendWithLevel(log.DebugLvl, v...) }
-func (self *LogStack) Info(v ...interface{})  { self.appendWithLevel(log.InfoLvl, v...) }
+func (self *LogStack) Trace(v ...interface{}) { self.appendWithLevel(seelog.TraceLvl, v...) }
+func (self *LogStack) Debug(v ...interface{}) { self.appendWithLevel(seelog.DebugLvl, v...) }
+func (self *LogStack) Info(v ...interface{})  { self.appendWithLevel(seelog.InfoLvl, v...) }
 
 func (self *LogStack) Warn(v ...interface{}) error {
-	return errors.New(self.appendWithLevel(log.WarnLvl, v...).text)
+	return errors.New(self.appendWithLevel(seelog.WarnLvl, v...).text)
 }
 func (self *LogStack) Error(v ...interface{}) error {
-	return errors.New(self.appendWithLevel(log.ErrorLvl, v...).text)
+	return errors.New(self.appendWithLevel(seelog.ErrorLvl, v...).text)
 }
 func (self *LogStack) Critical(v ...interface{}) error {
-	return errors.New(self.appendWithLevel(log.CriticalLvl, v...).text)
+	return errors.New(self.appendWithLevel(seelog.CriticalLvl, v...).text)
 }
 
 func (self *LogStack) traceWithCallDepth(callDepth int, message fmt.Stringer)    {}
@@ -86,13 +94,13 @@ func (self *LogStack) warnWithCallDepth(callDepth int, message fmt.Stringer)    
 func (self *LogStack) errorWithCallDepth(callDepth int, message fmt.Stringer)    {}
 func (self *LogStack) criticalWithCallDepth(callDepth int, message fmt.Stringer) {}
 
-func (self *LogStack) appendWithLevel(level log.LogLevel, params ...interface{}) *logMesage {
+func (self *LogStack) appendWithLevel(level seelog.LogLevel, params ...interface{}) *logMesage {
 	msg := logMesage{level: level, text: fmt.Sprint(params...)}
 	self.s = append(self.s, msg)
 	return &msg
 }
 
-func (self *LogStack) appendWithLevelf(level log.LogLevel, format string, params ...interface{}) *logMesage {
+func (self *LogStack) appendWithLevelf(level seelog.LogLevel, format string, params ...interface{}) *logMesage {
 	msg := logMesage{level: level, text: fmt.Sprintf(format, params...)}
 	self.s = append(self.s, msg)
 	return &msg
@@ -106,21 +114,30 @@ func (self *LogStack) Close() {
 func (self *LogStack) Flush() {
 	self.Lock()
 	defer self.Unlock()
+
+	var log seelog.LoggerInterface
+	if self.log != nil {
+		log = self.log
+	}
+	if self.loggerGetter != nil {
+		log = self.loggerGetter.GetLogger()
+	}
+
 	for len(self.s) > 0 {
 		message := self.s[0]
 		switch message.level {
-		case log.TraceLvl:
-			self.log.Trace(message.text)
-		case log.DebugLvl:
-			self.log.Debug(message.text)
-		case log.InfoLvl:
-			self.log.Info(message.text)
-		case log.WarnLvl:
-			self.log.Warn(message.text)
-		case log.ErrorLvl:
-			self.log.Error(message.text)
-		case log.CriticalLvl:
-			self.log.Critical(message.text)
+		case seelog.TraceLvl:
+			log.Trace(message.text)
+		case seelog.DebugLvl:
+			log.Debug(message.text)
+		case seelog.InfoLvl:
+			log.Info(message.text)
+		case seelog.WarnLvl:
+			log.Warn(message.text)
+		case seelog.ErrorLvl:
+			log.Error(message.text)
+		case seelog.CriticalLvl:
+			log.Critical(message.text)
 		}
 		self.s = self.s[1:len(self.s)]
 	}
